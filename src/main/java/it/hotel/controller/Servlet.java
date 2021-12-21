82d8f446f40e556a94fc51472d599a321986a396
Giovanni <g.depierro@studenti.unisa.it>
2021-12-21 14:29:07 +0100

    prova

--- src/main/java/it/hotel/controller/Servlet.java
+++ src/main/java/it/hotel/controller/Servlet.java
@@ -1,24 +1,16 @@
 package it.hotel.controller;
 
-import it.hotel.model.stanza.Stanza;
-import it.hotel.model.stanza.StanzaDAO;
-import it.hotel.model.stanza.stanzaExceptions.StanzaNotFoundException;
-
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
-import java.util.List;
 
 @WebServlet(name = "Servlet", value = "/Servlet")
 public class Servlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
-        StanzaDAO stanzaDAO = new StanzaDAO();
-        List<Stanza> stanze = (new StanzaDAO()).doSearch(true, null, null,
-                null, null, null, null, null);
-        response.getOutputStream().println("test" + stanze.size());
+        response.getOutputStream().println("test");
     }
 
     @Override
