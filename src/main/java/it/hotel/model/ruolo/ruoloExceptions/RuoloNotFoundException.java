2b247b1f2eff27bdef10d47ce25b9d7556ba7f38
Giovanni <g.depierro@studenti.unisa.it>
2021-12-21 20:17:00 +0100

    prova

--- src/main/java/it/hotel/model/ruolo/ruoloExceptions/RuoloNotFoundException.java
+++ src/main/java/it/hotel/model/ruolo/ruoloExceptions/RuoloNotFoundException.java
@@ -1,9 +1,21 @@
 package it.hotel.model.ruolo.ruoloExceptions;
 
+import it.hotel.model.ruolo.Ruolo;
+
+/**
+ * Segnala che non è stato trovato un oggetto {@link Ruolo} nel database.
+ */
 public class RuoloNotFoundException extends Exception{
 
+    /**
+     * Costruisce una RuoloNotFoundException senza un messaggio di dettagli.
+     */
     public RuoloNotFoundException() {}
 
+    /**
+     * Costruisce una RuoloNotFoundException con il messaggio di dettagli specificato.
+     * @param msg Il messaggio di dettagli
+     */
     public RuoloNotFoundException(String msg) { super(msg); }
 
 }
